101cd7ecfb023778d9de419fc653826934c4b7a1
cuadrapardo <74510425+cuadrapardo@users.noreply.github.com>
2025-11-22 19:14:54 +0100

    Create separate packages.nix file

---
+++ nixos/packages.nix
@@ -0,0 +1,22 @@
+# System wide packages
+{ pkgs }:
+
+with pkgs;
+
+  [
+    vim-full
+    # communication
+    discord
+    notion-app
+    # utils
+    tree
+    git
+    lshw
+    htop
+    pciutils
+    fastfetch
+    #modifications/theming
+    caffeine-ng #no sleep option in status bar
+    gnome-tweaks
+  ]
+
